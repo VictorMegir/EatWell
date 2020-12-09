4e1a0ae0ea5fe51ca0794c65715e19f54a9bc6ba
VicMeg <victormegir@gmail.com>
2020-12-09 22:53:02 +0200

    added images

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -4,10 +4,14 @@ function About()
 {
     return(
         <div className='about-page'>
-            <h2>ABOUT</h2>
-            <h3>Victor</h3>
-            <h3>TheMealDB.com</h3>
-            <h3>React</h3>
+            <div className='about-declaration'>ABOUT</div>
+            <div className='creator-info'>My name is Victor Megir. I like food and recipes and i believe we should all eat well. Contact me <a href='/contacts'>here.</a></div>
+            <div className='api-info'>
+                <div className='text'>This application was made possible thanks to <a href='https://www.themealdb.com/'>TheMealDB.com.</a></div>
+                <img src='./TheMealDB-image.png' alt='T_T' />
+                <div className='text'>This is a free web api that allows anyone to make web requests. It provides JSOn data for 254 recipes and 572 ingredients including images. These recipes and ingredients as well as the related images are the same as the ones my application provides. TheMealDB.com also provides the option to search recipes by their origin country, the ingredients they contain, and by their category they belong in. In addition it provides with a request call that selects a random recipe as well the option to search a recipe by their initial letter. </div>
+                </div>
+            <div className='tool-info'>React</div>
         </div>
     )
 }
