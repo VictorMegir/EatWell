366669e65d2ee9176ad4a33654a689f086ed6641
VicMeg <victormegir@gmail.com>
2020-12-10 02:43:47 +0200

    about contacts styles

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,17 +1,19 @@
+import './About.css';
 import React from 'react';
-
+import mealdbImage from './images/TheMealDB-image.png';
+import reactImage from './images/React-image.png';
 function About() 
 {
     return(
         <div className='about-page'>
             <div className='about-declaration'>ABOUT</div>
             <div className='creator-info'>My name is Victor Megir. I like food and recipes and i believe we should all eat well. Contact me <a href='/contacts'>here.</a></div>
-            <div className='api-info'>
-                <div className='text'>This application was made possible thanks to <a href='https://www.themealdb.com/'>TheMealDB.com.</a></div>
-                <img src='./TheMealDB-image.png' alt='T_T' />
-                <div className='text'>This is a free web api that allows anyone to make web requests. It provides JSOn data for 254 recipes and 572 ingredients including images. These recipes and ingredients as well as the related images are the same as the ones my application provides. TheMealDB.com also provides the option to search recipes by their origin country, the ingredients they contain, and by their category they belong in. In addition it provides with a request call that selects a random recipe as well the option to search a recipe by their initial letter. </div>
-                </div>
-            <div className='tool-info'>React</div>
+            <div className='api-info'>This application was made possible thanks to <a href='https://www.themealdb.com/'>TheMealDB.com.</a> This is a free web api that allows anyone to make web requests. It provides JSON data for 254 recipes and 572 ingredients including images. These recipes and ingredients as well as the related images are the same as the ones my application provides. TheMealDB.com also provides the option to search recipes by their origin country, the ingredients they contain, and by their category they belong in. In addition it provides with a request call that selects a random recipe as well the option to search a recipe by their initial letter.</div>
+            <div className='tool-info'>This application was build using <a href='https://reactjs.org/'>React.</a> React is a popular JavaScript framework that allows developers to easily build frontend applications and interfaces. This application utilizes the <a href='https://reactjs.org/docs/hooks-overview.html'>Hooks</a> programming interface, which simplifies the code. This application also utilizes the <a href='https://reactrouter.com/'>React Router</a> programming interface which allows for simplified navigation on the web.</div>
+            <div className='link-images'>
+                <a href='https://www.themealdb.com/'><img src={mealdbImage} className='mealdb-image' alt='T_T' /></a>
+                <a href='https://reactjs.org/'><img src={reactImage} className='react-image' alt='T_T' /></a>
+            </div>
         </div>
     )
 }
--- src/components/Pages/Contacts.js
+++ src/components/Pages/Contacts.js
@@ -1,13 +1,20 @@
+import './Contacts.css';
+import github from './images/github-image.png';
+import linkedin from './images/linkedin-image.png';
 import React from 'react';
 
 function Contacts() 
 {
     return(
         <div className='contacts-page'>
-            <h2>CONTACTS</h2>
-            <h3>Email</h3>
-            <h3>LinkedIn</h3>
-            <h3>GitHub</h3>
+            <div className='contacts-declaration'>CONTACTS</div>
+            <div className='email'>Contact me at <a href='mailto:victormegir@gmail.com'>victormegir@gmail.com</a></div>
+            <div className='linkedin'>Find me on LinkedIn as <a href='https://www.linkedin.com/in/victor-megir-246b8b188/'>Victor Megir</a></div>
+            <div className='github'>Find me on GitHub as <a href='https://github.com/VictorMegir'>Victor Megir</a></div>
+            <div className='link-images'>
+                <a href='https://www.linkedin.com/'><img className='linkedin-image' src={linkedin} alt='T_T'/></a>
+                <a href='https://github.com/'><img className='github-image' src={github} alt='T_T'/></a>
+            </div>
         </div>
     )
 }
